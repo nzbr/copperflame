b386b5d68a6d26fb0067f7f74b6c0c17310b9057
nzbr <mail@nzbr.de>
2025-10-14 00:20:06 +0200

    Patch with the nerdfonts patcher

--- font/default.nix
+++ font/default.nix
@@ -1,10 +1,54 @@
-{ iosevka
+{ stdenvNoCC
+, fetchzip
+, iosevka
+, fontforge
+, parallel
 , ...
 }:
+let
+  unpatched = (iosevka.override (args: {
+    set = "copperflame";
+    privateBuildPlan = builtins.readFile ./build-configuration.toml;
+  })).overrideAttrs (args: {
+    pname = "CopperflameMono";
+  });
+in
+stdenvNoCC.mkDerivation {
+  name = "CopperflameMono Nerd Font";
 
-(iosevka.override (args: {
-  set = "copperflame";
-  privateBuildPlan = builtins.readFile ./build-configuration.toml;
-})).overrideAttrs (args: {
-  pname = "CopperflameMono";
-})
+  src = fetchzip {
+    url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FontPatcher.zip";
+    stripRoot = false;
+    hash = "sha256-koZj0Tn1HtvvSbQGTc3RbXQdUU4qJwgClOVq1RXW6aM=";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ fontforge parallel ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    parallel -j $NIX_BUILD_CORES fontforge --script $src/font-patcher --complete ::: ${unpatched}/share/fonts/truetype/*.ttf
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/fonts/truetype
+    cp -vr ${unpatched}/share/fonts/truetype/. $out/share/fonts/truetype
+    cp -v *.ttf $out/share/fonts/truetype
+
+    runHook postInstall
+  '';
+
+  preFixup = ''
+    chmod -x $out/share/fonts/truetype/*.ttf
+  '';
+
+  passthru = {
+    inherit unpatched;
+  };
+}
